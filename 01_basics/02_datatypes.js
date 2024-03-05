ffb74dce5de34778d38946bf0d375ef16844891f
Nayaab Zameer Qazi <148606115+nybzmr@users.noreply.github.com>
2024-03-05 06:58:46 +0000

    Datatypes discussion.

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,25 @@
+"use strict"; // treat all JS code as newer version // good practice 
+
+
+// alert("This is an alert popup")  // we r using node js not, the browser 
+
+console.log(3
+    +3); //permissible in node but not reccommended as code readability should be high
+
+console.log("Nayaab");
+
+
+let name ="Nayaab";  //string
+let age = 19;  // int
+let isLoggedIn = false;  //boolean
+
+
+// null => stand alone value, empty value but a value
+// undefined => not defined;
+// symbol => unique
+
+
+// object
+
+console.table([typeof("This is a string"), typeof(null), typeof(undefined)])
+// typeof null is object
