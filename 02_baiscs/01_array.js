07e909d60a78d62eef3ce9d80edf8e2988dbb9d3
Nayaab Zameer Qazi <148606115+nybzmr@users.noreply.github.com>
2024-03-05 18:01:17 +0000

    Discussed about arrays and builtin methods of arrays.

---
+++ 02_baiscs/01_array.js
@@ -0,0 +1,20 @@
+// //Array
+const myArr = [1, 2, 3, 4, 5, 6];
+// const myHeros = ["IronMan", "Thor", "SpiderMan", "Dr. Strange"]
+
+// const diffDeclaration = new Array(1,2,3,4,5);
+// console.log(myArr[0]);
+
+// // Array methods
+// myArr.push(7);
+// myArr.push(8);
+// myArr.pop();
+
+// myArr.unshift(0);
+// myArr.shift();
+
+console.log(myArr.includes(9));
+console.log(myArr.indexOf(9));
+
+const newArray = myArr.join(); //Converts to string
+console.log(typeof newArray);
