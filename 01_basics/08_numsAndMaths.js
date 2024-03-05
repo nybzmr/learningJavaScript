789a952bcb117c8ce29e469cde801c24f62c1a13
Nayaab Zameer Qazi <148606115+nybzmr@users.noreply.github.com>
2024-03-05 12:24:06 +0000

    Discussed about declaration and methods of Number. Also discussed Math object and Math.random in detail.

---
+++ 01_basics/08_numsAndMaths.js
@@ -0,0 +1,29 @@
+// const score = 400;
+
+// const balance = new Number(100);
+// console.log(balanced);
+
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+
+
+// const otherNumber = 123.03924375;
+// console.log(otherNumber.toPrecision(3))
+
+
+//********************** MATHS**************************
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.3)); //rounds off the value to nearest integer
+// console.log(Math.ceil(4.3)); // rounds off to nearest greater integer
+// console.log(Math.floor(4.3));// rounds off to nearest lesser integer
+
+// console.log(Math.min(4 , 8, 6 , 3, 9));
+// console.log(Math.max(4 , 8, 6 , 3, 9));
+
+// console.log(Math.random());// gives any random number between 0 and 1
+
+// Generate random number bw 10 and 20;
+const min = 10;
+const max = 20
+console.log(Math.floor(((Math.random()*(max-min))+1))+min)
