a2df8b9bcec9bf94c792278922f2392e3ff32769
Nayaab Zameer Qazi <148606115+nybzmr@users.noreply.github.com>
2024-03-11 08:40:43 +0000

    More disvussion on arrays.

---
+++ 02_baiscs/02_array.js
@@ -0,0 +1,20 @@
+const marvelHeros = ["IronMan", "Thor", "SpiderMan", "Dr. Strange"]
+const dcHeros = ["SuperMan", "Flash", "Batmam"]
+
+//  marvelHeros.push(dcHeros);
+//  console.log(marvelHeros[4][1]);
+
+// const allHeros = marvelHeros.concat(dcHeros);
+// console.log(marvelHeros)
+// console.log(allHeros);
+
+//Spread 
+// const allHeros = [...marvelHeros, ...dcHeros];
+// console.log(allHeros)
+
+
+//flat
+// const newArray =[1,2,3,[4,5,6],7,[6,7,[4,5]]]
+// const flatArray = newArray.flat(Infinity);
+// console.log(flatArray)
+
