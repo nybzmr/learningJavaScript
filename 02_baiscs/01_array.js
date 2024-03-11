5826400a1724a65587b540adb681915df56d68ed
Nayaab Zameer Qazi <148606115+nybzmr@users.noreply.github.com>
2024-03-11 08:23:33 +0000

    Discussed about arrays and their methods.

--- 02_baiscs/01_array.js
+++ 02_baiscs/01_array.js
@@ -13,8 +13,22 @@ const myArr = [1, 2, 3, 4, 5, 6];
 // myArr.unshift(0);
 // myArr.shift();
 
-console.log(myArr.includes(9));
-console.log(myArr.indexOf(9));
+// console.log(myArr.includes(9));
+// console.log(myArr.indexOf(9));
 
-const newArray = myArr.join(); //Converts to string
-console.log(typeof newArray);
+// const newArray = myArr.join(); //Converts to string
+// console.log(typeof newArray);
+
+
+
+//Concept of splice and slice
+// Slice doesnt includes the last  element of the range and keeps the original array unaffected.
+// Splice includes the last element of the range and changes the original array.
+
+const slicedArray = myArr.slice(1,3)
+console.log("Sliced array"+slicedArray);// prints 2,3
+console.log(myArr); // prints 1, 2 , 3, 4, 5, 6
+
+const splicedArray = myArr.splice(1,3)
+console.log("Spliced array"+splicedArray);// prints 2, 3, 4
+console.log(myArr); // prints 1, 5, 6
