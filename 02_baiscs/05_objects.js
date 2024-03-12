fd8142ec596fa1a92889ff2c929ab3cd6ad884fd
Nayaab Zameer Qazi <148606115+nybzmr@users.noreply.github.com>
2024-03-12 07:54:32 +0000

    Discussed destructuring of objects.

---
+++ 02_baiscs/05_objects.js
@@ -0,0 +1,15 @@
+// Destructuring
+
+ const course = {
+    courseName : "js in hindi",
+    price: 999,
+    courseStudent : "nayaab"
+ }
+
+ const {courseStudent: student} = course;
+
+
+//  {
+//    "name" : "Nayaab",
+//    "CourseName ": "JS in hindi"
+//  }
