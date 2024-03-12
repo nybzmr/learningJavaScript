0f15fa3409c2150444f6526f99fc07c7a990dc2b
Nayaab Zameer Qazi <148606115+nybzmr@users.noreply.github.com>
2024-03-12 05:19:02 +0000

    More about objects.

---
+++ 02_baiscs/04_objects.js
@@ -0,0 +1,76 @@
+// const tinderUser = new Object();
+
+
+// const tinderUser ={}
+// tinderUser.id = "nyb_zmr";
+// tinderUser.name = "Nayaab Zameer"
+// tinderUser.emailId = "nybzmr02@gmail.com"
+
+
+// const tinderUser ={
+//     email : "nybzmr02@gmail.com",
+//     fullName: {
+//         userFullname: {
+//             firstName : "Nayaab",
+//             lastName : "Zameer",
+//         }
+//     }
+// }
+
+
+// console.log(tinderUser.fullName.userFullname.lastName);
+
+// const obj1 ={
+//     1 : "a",
+//     2: "b",
+// }
+
+
+// const obj2 ={
+//     3 : "c",
+//     4: "d",
+// }
+
+// const obj3 = Object.assign({}, obj1, obj2);
+// console.log(obj3);
+
+
+// const obj1 ={
+//     1 : "a",
+//     2: "b",
+// }
+
+
+// const obj2 ={
+//     3 : "c",
+//     4: "d",
+// }
+
+// const obj3 = {...obj1, ...obj2};
+
+
+
+
+// Tackling database values
+// const users = [
+//     {
+//         id: 1,
+//         email: "n@gmail.com",
+
+//     }
+// ]
+
+// console.log(users[0].email);
+
+
+
+
+const tinderUser ={}
+tinderUser.id = "nyb_zmr";
+tinderUser.name = "Nayaab Zameer"
+tinderUser.emailId = "nybzmr02@gmail.com"
+
+console.log(Object.keys(tinderUser));
+console.log(Object.values(tinderUser));// returns all the keys and values in array format.
+console.log(Object.entries(tinderUser));// returns array of arrays of each key and value.
+
