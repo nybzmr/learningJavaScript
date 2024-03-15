e66cdf6ced23ca28d0e5c1c7d4248f2f38c0ebdc
Nayaab Zameer Qazi <148606115+nybzmr@users.noreply.github.com>
2024-03-15 09:24:29 +0000

    Discussed about syntax this keywird and arrow functions.

---
+++ 03_basics/02_arrowFunctions.js
@@ -0,0 +1,32 @@
+// const user = 
+// {
+//     username: "hitesh",
+//     price : 999,
+
+//     welcomeMessage: function(){
+//         console.log(`${this.username} , Welcome to the website!!!`)
+//     }
+// }
+// // this refers to current context
+
+
+
+//Arrow function
+// const myfunction = ()=> {
+//     console.log("This is an arrow function")
+// }
+// myfunction()
+
+
+// implicit return
+// const addTwo = (num1 , num2) => num1 + num2;
+// console.log(addTwo(4,5));
+
+//using paranthesis
+// const addTwo = (num1 , num2) => (num1 + num2);
+// console.log(addTwo(4,5));
+
+
+// returning object
+const returnObject = ()=> ({username : "nyb_zmr"})
+console.log(returnObject().username)
