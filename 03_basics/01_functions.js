7d66fb67b6e57dc9f714e2bfb011abbbfd973619
Nayaab Zameer Qazi <148606115+nybzmr@users.noreply.github.com>
2024-03-15 08:42:25 +0000

    Dicussed functions in  depth.

---
+++ 03_basics/01_functions.js
@@ -0,0 +1,80 @@
+// console.log("N");
+// console.log("a");
+// console.log("y");
+// console.log("a");
+// console.log("a");
+// console.log("b");
+
+
+// function sayMyName() {
+//     console.log("N");
+//     console.log("a");
+//     console.log("y");
+//     console.log("a");
+//     console.log("a");
+//     console.log("b");    
+// }
+
+// sayMyName();
+
+
+
+// // Function to add two numbers
+// function addTwoNumbers(num1, num2){ //num1 and num2 are parameters
+//     console.log(num1+num2);
+// }
+
+// addTwoNumbers(3,5);//3 and 5 are arguments
+// function cout(parameter){
+//     console.log(parameter);
+// }
+// function addTwonumbers(num1,num2){
+//     // let sum = num1+ num2;
+//     // return sum;
+
+//     return num1+num2;
+// }
+// const result = addTwonumbers(5,8)
+// cout(result);
+
+
+
+
+// default parameter
+// function lgoinUserMessage(username = "sam"){
+//     console.log(`${username} just logged in`);
+//     return;
+// }
+// lgoinUserMessage("Nayaab");// prints Nayaab just logged in
+// lgoinUserMessage();// prints Sam just logged in
+
+
+
+
+
+// function calculateCartValue(...itemValue){
+//     let cartValue = 0;
+//     for(let i=0; i<itemValue.length; i++){
+//         cartValue = cartValue + itemValue[i];
+//     }
+//     return cartValue;
+// }
+
+// let cartValue = calculateCartValue(100,300,500,200,100,200);
+// console.log(cartValue);
+
+
+
+
+
+//Handeling objects
+const user = {
+    username: "Nayaab",
+    password: 12345
+}
+
+function handleObject(anyObject){
+    console.log(`Your username is ${anyObject.username} and your password is ${anyObject.password}`);
+}
+
+handleObject(user);
