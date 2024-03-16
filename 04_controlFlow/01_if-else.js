c3352f41ffe471bafb874848d38ae1dff0b1bb1d
Nayaab Zameer Qazi <148606115+nybzmr@users.noreply.github.com>
2024-03-16 06:28:32 +0000

    Disucssed in depth about if-else switch-case and truthy and falsy values.

---
+++ 04_controlFlow/01_if-else.js
@@ -0,0 +1,31 @@
+// if
+
+// let condition = true;
+// if(condition)// this condition must be true for execution of this if statement
+// {
+//     console.log("Condition is true!");
+// }
+// <,>,<=,>=,==,===
+// == implicityly converts datatypes before comparison. 2=='2' is true.
+// === is strict equal which doesnt implicitly convert the datatypes. 2 === '2' is false.
+
+//else
+// else{
+//     console.log("Condition is false");
+// }
+
+
+
+// shorthand 
+const balance = 1000;
+// if (balance > 500) console.log("Sufficient balance");
+
+// if(balance < 500){
+//     console.log("Less than 500");
+// }
+// else if(balance>500&&balance<1000){
+//     console.log("Greater than 500 but less than 1000");
+// }
+// else{
+//     console.log("Greater than 1000");
+// }
